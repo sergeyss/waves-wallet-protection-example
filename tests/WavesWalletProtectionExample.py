a73d5b07b4ce85e67295c1900cb9cdd8dcde2753
Sergey Valiev <123@456.email>
2021-02-09 11:37:01 +0300

    set configuration

--- tests/WavesWalletProtectionExample.py
+++ tests/WavesWalletProtectionExample.py
@@ -1,5 +1,26 @@
 import pywaves
+import requests
 
 
 class PyWaves2Sig(pywaves.ParallelPyWaves):
-    pass
+
+    def __init__(self):
+        super.__init__()
+
+        self.NODE = 'https://nodes-testnet.wavesnodes.com/'
+        self.CHAIN = 'testnet'
+        self.CHAIN_ID = 'T'
+
+    def wrapper(self, api, postData='', host='', headers=''):
+        # global OFFLINE
+        if self.OFFLINE:
+            offlineTx = {'api-type': 'POST' if postData else 'GET', 'api-endpoint': api, 'api-data': postData}
+            return offlineTx
+        if not host:
+            host = self.NODE
+        if postData:
+            req = requests.post('%s%s' % (host, api), data=postData,
+                                headers={'content-type': 'application/json'}).json()
+        else:
+            req = requests.get('%s%s' % (host, api), headers=headers).json()
+        return req
